34028017b4137294dbc202ecbeb4720a3b3d043b
larysamarchuk <133905316+larysamarchuk@users.noreply.github.com>
2023-06-18 21:04:56 +0200

    Add files via upload

---
+++ scan3.java
@@ -0,0 +1,20 @@
+package Homework.New;
+public class scan3 {
+    public static void main(String[] args) {
+        char charVariable1 = '0';
+        char charVariable2 = 0;
+        char charVariable3 = '\0';
+
+
+        char charValue = '2';
+        int intValue = charValue - '0';
+
+
+
+
+    }
+
+
+
+        }
+
