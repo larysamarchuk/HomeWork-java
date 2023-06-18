8f2199f0f4ab5eebcf13e3fb415619ce02f1ba7f
larysamarchuk <133905316+larysamarchuk@users.noreply.github.com>
2023-06-18 21:06:59 +0200

    Add files via upload

---
+++ skan2.java
@@ -0,0 +1,12 @@
+package Homework.New;
+
+public class skan2 {
+    public static void main(String[] args) {
+        float result1 = (float) (Math.pow(Math.E, 5) + 1.5);
+
+        int result2 = (byte) (Math.log(2) + (short) 14);
+
+        double result3 = 1 + 1.5;
+
+    }
+}
